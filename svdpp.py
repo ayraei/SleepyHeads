9498c1b5f865829cb7bc82ecba3ab1b1e2d4836c
Debbie <htsai2@caltech.edu>
2015-05-21 01:06:59 -0700

    Debbie: fixing typos

--- svdpp.py
+++ svdpp.py
@@ -8,8 +8,8 @@ NUM_MOVIES   = 17770
 NUM_FEATURES = 20
 
 # Run parameters
-LEARNING_RATE = 0.0002
-REG_PENALTY   = 0.04
+LEARNING_RATE = 0.001
+REG_PENALTY   = 0.015
 NUM_EPOCHS    = 2
 
 # Location of input files 
@@ -20,12 +20,6 @@ TEST_FILE_LOC  = '/Users/debbie1/Documents/NetflixData/mu_sorted/probe.dta'
 OUTPUT_PREDICT_LOC = '/Users/debbie1/Documents/NetflixData/output/SVDPP_predictions_probe.dta'
 OUTPUT_PERF_LOC    = '/Users/debbie1/Documents/NetflixData/output/SVDPP_performance.dta'
 
-# RateUnit
-class RateUnit:
-    def __init__(self, ID, rating):
-        self.getID = ID
-        self.getRating = rating
-
 # ArrayManager
 class ArrayManager:
     def __init__(self):
@@ -34,26 +28,24 @@ class ArrayManager:
         self.map  = {}
 
     def add(self, userID, movieID, rating):
-        ru = RateUnit(movieID, rating)
-
         if userID in self.map:
-            self.map[userID].append(ru)
+            np.concatenate((self.map[userID], [[movieID, rating]]))
         else:
-            self.map[userID] = [ru]
+            self.map[userID] = np.array([[movieID, rating]])
     
     def getUserHistory_N(self, userID):
         return self.map.get(userID)
 
     def getSum(self, userID):
-        return sums[userID]
+        return self.sums[userID]
 
     def getN(self, userID):
-        return Ns[userID]
+        return self.Ns[userID]
 
     def getOriginalRating(self, userHistory, movieID):
-        for ru in userHistory:
-            if ru.getID == movieID:
-                return ru.getRating
+        for pair in userHistory:
+            if pair[0] == movieID:
+                return pair[1]
         return 0.0
 
     def initConstants(self):
@@ -61,8 +53,8 @@ class ArrayManager:
             userHistory = self.getUserHistory_N(i);
             self.Ns[i] = math.pow(len(userHistory), -0.5)
 
-            for ru in userHistory:
-                self.sums[i] += ru.getRating
+            for pair in userHistory:
+                self.sums[i] += pair[1]
 
 # Initialization
 def init(arrayManager):
@@ -96,7 +88,6 @@ def init(arrayManager):
     arrayManager.initConstants()
 
 def predictedRating(arrayManager, userID, movieID):
-    maxIndex = NUM_FEATURES - 1
     N = arrayManager.getN(userID)
     N_list = arrayManager.getUserHistory_N(userID)
     q_i = q[movieID, 0:]
@@ -104,11 +95,9 @@ def predictedRating(arrayManager, userID, movieID):
     y_sum = np.zeros(NUM_FEATURES)
 
     # Calculate y_sum (userTestRatings of all the ratings from a user)
-    for ru in N_list:
-        movie = ru.getID
-
-        for f in range(0, maxIndex):
-            y_sum[f] += y[movieID][f]
+    for pair in N_list:
+        movie = pair[0]
+        y_sum += y[movie, 0:]
     y_sum *= N
 
     # r hat = q[movie] * (pu + N * sum(yj))
@@ -124,27 +113,27 @@ def main():
     y = 0.001 * np.random.randn(NUM_MOVIES, NUM_FEATURES)
 
     # Initialize everything else
+    print "Start: %s" % str(dt.datetime.now().time())
     arrayManager = ArrayManager()
     init(arrayManager)
-    print "Initialization complete!\n"
+    print "Initialization complete at time %s \n" % str(dt.datetime.now().time())
 
     # File prep
     f_perf  = open(OUTPUT_PERF_LOC, 'w')
     f_train = open(TRAIN_FILE_LOC,  'r')
 
-    # Constants
-    maxIndex = NUM_FEATURES - 1
-    LRtRP = LEARNING_RATE * REG_PENALTY
-
     # SGD
     for e in range(0, NUM_EPOCHS):
+
+        LRtRP = LEARNING_RATE * REG_PENALTY
         print "Epoch %d start: %s \n" % (e, str(dt.datetime.now().time()))
 
         for count, line in enumerate(f_train):
 
             # Print progress through file
-            if count % 500000 == 0:
+            if count % 10000000 == 0:
                 print count
+                print "%s \n" % str(dt.datetime.now().time())
 
             # Get training data
             lineArray = line.split()
@@ -160,45 +149,43 @@ def main():
             y_sum = np.zeros(NUM_FEATURES)
 
             # Calculate y_sum (sum of all the ratings from a user)
-            for ru in N_list:
-                movie = ru.getID
-
-                for f in range(0, maxIndex):
-                    y_sum[f] += y[movieID][f]
+            for pair in N_list:
+                movie = pair[0]
+                y_sum += y[movie, 0:]
             y_sum *= N
 
             # Calculate error
-            rHat = (q_i * (y_sum + p_u))[0]
-            err = rating - rHat
+            err  = rating - (q_i * (y_sum + p_u))[0]
             LRtE = LEARNING_RATE * err
 
             # Print out performance
-            f_perf.write("%f %f %f %f \n" % (userID, movieID, rating, rating))
+            if count % 100 == 0:
+                f_perf.write("%d %d %d %f \n" % (userID, movieID, rating, err))
 
             # Update q
             # q_i = q_i + LEARNING_RATE * err * (p_u + y_sum) - LEARNING_RATE * REG_PENALTY * q_i
-            q_temp = q_i + LRtE * (p_u + y_sum) - LRtRP * q_i
-            q[movieID, 0:] = q_temp
+            q[movieID, 0:] += LRtE * (p_u + y_sum) - LRtRP * q_i
 
             # Update p
             # p_u = p_u + LEARNING_RATE * err * q_i - LEARNING_RATE * REG_PENALTY * p_u
-            p_u += LRtE * q_i - LRtRP * p_u
-            p[userID, 0:] = p_u
+            p[userID, 0:] += LRtE * q_i - LRtRP * p_u
 
             # Update y
             # y_j = y_j + q_i * LEARNING_RATE * err * N - LEARNING_RATE * REG_PENALTY * y_j
             y[movieID, 0:] += LRtE * N * q_i - LRtRP * y[movieID, 0:]
 
-            print "Epoch %d ended: %s \n" % (e, str(dt.datetime.now().time()))
+        print "Epoch %d ended: %s \n" % (e, str(dt.datetime.now().time()))
+        LEARNING_RATE *= 0.9;
 
     f_perf.close()
     f_train.close()
 
     # Write out predictions
-    f_test = open(OUTPUT_PREDICT_LOC, 'w')
-    f_out = open(OUTPUT_PREDICT_LOC, 'w')
+    f_test = open(TEST_FILE_LOC, 'w')
+    f_out  = open(OUTPUT_PREDICT_LOC, 'w')
 
-    for count, line in enumerate(f_train):
+    for count, line in enumerate(f_test):
+
         # Print progress through file
         if count % 500000 == 0:
             print count
